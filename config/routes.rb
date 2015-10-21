59c1b6bd120094ce26ff20c0358c361b650c2017
Gunjan Patel <gpatel122@gmail.com>
2015-10-21 20:48:00 +0000

    Set up controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FantasyQuiz::Application.routes.draw do
+
+  root 'leagues#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
