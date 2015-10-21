25cd21c36d2e929d5b5184e6907a4bf08f1943ea
Gunjan Patel <gpatel122@gmail.com>
2015-10-21 20:03:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FantasyQuiz::Application.config.secret_key_base = '624616510708bbb353e019cc23ca7c4e6105ce01f5be41a9d660d32dac6c5376c68c02c8350005686c552f9a59dfd2e7ef8d0927dba2a0a075dd01d8a81a5386'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FantasyQuiz::Application.config.session_store :cookie_store, key: '_Fantasy-Quiz_session'
